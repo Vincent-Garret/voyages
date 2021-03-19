5fcfc8977f7c75720c22ea02a998ff91ba408ffb
JVincent Garret <vincentgarret33@gmail.com>
2021-03-19 17:14:16 +0100

    first tables--migration

---
+++ src/Entity/Trip.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TripRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TripRepository::class)
+ */
+class Trip
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=1000)
+     */
+    private $poetry;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPoetry(): ?string
+    {
+        return $this->poetry;
+    }
+
+    public function setPoetry(string $poetry): self
+    {
+        $this->poetry = $poetry;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
