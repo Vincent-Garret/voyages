6eb32beec8c7248e1b736e3dd382fd10fa1335b4
JVincent Garret <vincentgarret33@gmail.com>
2021-03-19 18:26:08 +0100

    READ from CRUD

---
+++ src/Controller/Admin/CategoryController.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Controller\Admin;
+
+
+use App\Entity\Category;
+use App\Entity\Trip;
+use App\Repository\CategoryRepository;
+use App\Repository\TripRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Form;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class CategoryController extends AbstractController{
+
+    /**
+     * @Route("/admin/categorylist", name="categoryList" )
+     */
+    public function category(CategoryRepository $categoryRepository)
+    {
+        $categories = $categoryRepository->findAll();
+
+        return $this->render('Admin/category.html.twig', [
+            'categories' => $categories
+        ]);
+    }
+    
+}
