4fc81bdb9cd0d72c79c23cd0ac80cdb72358c329
JVincent Garret <vincentgarret33@gmail.com>
2021-03-30 12:02:24 +0200

    adding options in RegistrationType

--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -4,9 +4,12 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 
 class RegistrationType extends AbstractType
@@ -14,9 +17,22 @@ class RegistrationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username')
-            ->add('password')
-            ->add('submit', SubmitType::class)
+            ->add('username', TextType::class, [
+                'label' => "Nom d'utilisateur"
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'label' => 'Mot de passe',
+                'invalid_message' => 'le mot de passe n\'est pas identique',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options' => ['label' => 'mot de passe'],
+                'second_options' => ['label' => 'Confirmer le mot de passe'],
+                'mapped' => false
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Je m\'inscris !'
+            ])
         ;
     }
 
